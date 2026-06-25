69a6413ccfd0607e0cf50755fbfec4a43f2de7e3
415-25-kitipoom <56430@yupparaj.ac.th>
2026-06-25 08:58:27 +0700

    digital_profile.py

---
+++ digital_profile.py
@@ -0,0 +1,12 @@
+import streamlit as st
+
+st.title("🐱‍🏍 MY Digtal Profile")
+
+st.header("🐱‍💻 สวัดดีครับ")
+st.write("นายกิติภูมิ วงค์สอน')    
+st.write("ชื่อเล่น ไฮคิว") 
+st.write(:เลขที่ 25 ม.4/15") 
+
+st.header("🌹 เป้าหมายและสิ่งที่อยากทําในอนาคต")
+st.write("เป้าหมาย : **อยากไปเรียนต่างประเทศ")
+st.write("สิ่งที่อยากเป็นในอนาคต : นักการทูต (Diplomat)")
