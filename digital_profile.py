9bf2498b5a690e90742795fdd88e196920fa435d
415-25-kitipoom <56430@yupparaj.ac.th>
2026-06-25 09:08:30 +0700

    Update digital_profile.py

--- digital_profile.py
+++ digital_profile.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.title("🐱‍🏍 MY Digtal Profile")
 
 st.header("🐱‍💻 สวัดดีครับ")
-st.write("นายกิติภูมิ วงค์สอน')    
+st.write("นายกิติภูมิ วงค์สอน")    
 st.write("ชื่อเล่น ไฮคิว") 
 st.write(:เลขที่ 25 ม.4/15") 
 
