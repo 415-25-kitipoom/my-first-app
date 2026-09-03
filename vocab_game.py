d0f1abf4e267048b53d178b4f276a0e3eb15d557
415-25-kitipoom <56430@yupparaj.ac.th>
2026-09-03 09:01:33 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -9,9 +9,9 @@ if "ans1_val" not in st.session_state:
 if "ans2_val" not in st.session_state:
     st.session_state.ans2_val = ""
 if "ans3_val" not in st.session_state:
-    st.session_state.ans1_val = ""
+    st.session_state.ans3_val = ""
 if "ans4_val" not in st.session_state:
-    st.session_state.ans2_val = ""
+    st.session_state.ans4_val = ""
 
 # 📌 ฟังก์ชันเคลียร์ค่าเมื่อกดปุ่มเริ่มใหม่
 def reset_game():
@@ -33,8 +33,8 @@ def show_result_dialog(ans1, ans2):
 
     u_ans1 = ans1.strip().lower()
     u_ans2 = ans2.strip().lower()
-    u_ans3 = ans1.strip().lower()
-    u_ans4 = ans2.strip().lower()
+    u_ans3 = ans3.strip().lower()
+    u_ans4 = ans4.strip().lower()
 
     # ตรวจข้อ 1
     if u_ans1 == "apple":
@@ -53,14 +53,14 @@ def show_result_dialog(ans1, ans2):
     # ✏️ [พื้นที่สำหรับนักเรียน]: เพิ่มตรวจข้อ 3, 4 ตรงนี้
     
     # ตรวจข้อ 3
-    if u_ans2 == "banana":
+    if u_ans3 == "banana":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
        st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
     # ตรวจข้อ 4
-    if u_ans2 == "lemon":
+    if u_ans4 == "lemon":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
@@ -69,7 +69,7 @@ def show_result_dialog(ans1, ans2):
 
     st.info(f"🏆 ได้คะแนนรวม: {score} คะแนน")
 
-    if score == 2:
+    if score == 4:
         st.success("🎉 You win!")
     else:
         st.error("💀 You lose!")
@@ -112,11 +112,11 @@ st.session_state.ans4_val = ans4
 
 ans3 = st.text_input(
     "ข้อ 3: monkey love to eat `b _ _ a n a`. 🍌",
-    value=st.session_state.ans2_val,
+    value=st.session_state.ans3_val,
 )  
  ans4 = st.text_input(
     "ข้อ 4: i love to eat `l _ m o n`. 🍋",
-    value=st.session_state.ans2_val,
+    value=st.session_state.ans4_val,
 
 
 # 4. ปุ่มส่งคำตอบ
@@ -130,7 +130,7 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 # 5. แสดง Dialog ผลลัพธ์
 if st.session_state.get("is_ended", False):
-    show_result_dialog(ans1, ans2)
+    show_result_dialog(ans1, ans2 , ans3 , ans4)
 
 st.divider()
 st.write("นายกิติภูมิ วงค์สอน เลขที่ 25 ม.4/5")
